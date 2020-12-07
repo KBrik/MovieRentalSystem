59c1fafc04588908c837b5c11a68945917193f24
Kateryna Brik <33495900+KBrik@users.noreply.github.com>
2020-12-07 19:12:23 +0200

    Add files via upload

---
+++ MovieRentalSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MovieRentalSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignUp su = new SignUp();
+            Console.WriteLine(su.NewUserSingUp("user@gmail.com", "dffbhQdf2sdg"));
+        }
+    }
+}
---
+++ MovieRentalSystem/SignUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MovieRentalSystem
+{
+    public class SignUp
+    {
+        public int NewUserSingUp(string username, string password)
+        {
+            Regex regex1 = new Regex(@"(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)");
+            if (regex1.Matches(username).Count == 0)
+                return -1;
+
+            Regex regex2 = new Regex(@"(^\w*([0-9]+[a-z]+[A-Z]+)|([0-9]+[A-Z]+[a-z]+)|([A-Z]+[a-z]+[0-9]+)|([A-Z]+[0-9]+[a-z]+)|([a-z]+[0-9]+[A-Z]+)|([a-z]+[A-Z]+[0-9]+)\w*$)");
+            if (regex2.Matches(password).Count == 0)
+                return -2;
+
+            return 0;
+        }
+    }
+}
